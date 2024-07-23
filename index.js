9c66266034b49826ae4a1c9ca5a5af84602f2fef
Kiriill Kagdin <rlyiamberserker@gmail.com>
2024-07-23 09:24:13 +0300

    remove js

--- index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var HashTable_1 = require("./Data Structures and Algo/HashTable");
-var validanagram_eqsy_1 = require("./LeetCode/hash/validanagram-eqsy");
-var table = new HashTable_1.HashTable();
-var a = table.hash("anagram");
-var b = table.hash("nagaram");
-console.log("COMPARE", a, b);
-console.log((0, validanagram_eqsy_1.isAnagram)("anagram", "nagaram"));
--- src/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const SameTree_easy_1 = require("./LeetCode/graphs/SameTree-easy");
-const _2occMaxStrEz_1 = require("./LeetCode/search/slidingWindow/2occMaxStrEz");
-const nodeA = new SameTree_easy_1.TreeNode(1, new SameTree_easy_1.TreeNode(2), new SameTree_easy_1.TreeNode(3));
-const nodeB = new SameTree_easy_1.TreeNode(1, new SameTree_easy_1.TreeNode(2), new SameTree_easy_1.TreeNode(3));
-console.log((0, _2occMaxStrEz_1.maximumLengthSubstring)("ccbcb"));
