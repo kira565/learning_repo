87c52c203ade2d4ca1725504fa9f33b3afc78024
Kiriill Kagdin <rlyiamberserker@gmail.com>
2024-09-05 17:34:03 +0300

    leetcode

---
+++ src/LeetCode/backtracking/generate-parenheses-m.ts
@@ -0,0 +1,45 @@
+//* Given: n pairs of parentheses "()" - pair
+// ? Write a function to generate all combinations of well-formed parentheses
+
+//Input: n = 3
+//Output: ["((()))","(()())","(())()","()(())","()()()"]
+
+// backtracking, dynamic programming
+//! constraits:
+// 1 <= n <= 8
+// well-formed (())
+// not well-formed  )()(, (())())
+// ()()()-close immidiately
+// ((())) - close later
+// ()
+// [(, (, (, ),),)]
+
+// 1. ()()() - дефолт, все скобки по отдельности
+
+//Берем одну скобку оборачиваем оставшиеся в нее разными вариантами: ()()   <-- ()
+
+// (())() первая
+// ()(()) вторая
+// (()()) обе
+
+//? если бы было больше;
+// третья ()()(())
+//
+
+// борачиваем в 2 скобки () <--- (())
+//((()))
+
+const s = "((()))";
+function generateParenthesis(n: number): string[] {
+  if (n < 1) return [];
+  const output: string[] = [];
+  const initial = Array(n).fill("()"); // ["()", "()", "()"] 3
+  console.log(initial);
+
+  function buildQuery(current: string[]) {
+    // ""
+    // (()())
+  }
+
+  return output;
+}
