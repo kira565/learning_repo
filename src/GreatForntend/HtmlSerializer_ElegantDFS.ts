76e39d14f4ebdc2f9b72120a9531e42280c56a9d
Kiriill Kagdin <rlyiamberserker@gmail.com>
2025-11-12 14:03:08 +0200

    fx

--- src/GreatForntend/HtmlSerializer_ElegantDFS.ts
+++ src/GreatForntend/HtmlSerializer_ElegantDFS.ts
@@ -4,17 +4,17 @@ type Element = { tag: string; children: Array<string | Element> };
 export default function serializeHTML(element: Element): string {
   const resultingArray: string[] = [];
 
-  function addTag(tag: string, level: number, isTag = true, isOpen = true) {
+  function createTag(tag: string, level: number, isTag = true, isOpen = true) {
     return `${new Array(level).fill("\t").join("")}${isTag ? (isOpen ? `<${tag}>` : `</${tag}>`) : tag}`;
   }
 
   function dfs(node: Element | string, level: number) {
     if (typeof node === "string") {
-      resultingArray.push(addTag(node, level, false));
+      resultingArray.push(createTag(node, level, false));
     } else {
-      resultingArray.push(addTag(node.tag, level, true, true)); // идем в глубину (открываем теги)
+      resultingArray.push(createTag(node.tag, level, true, true)); // идем в глубину (открываем теги)
       node.children.forEach((child) => dfs(child, level + 1)); // go deeper (recursive dfs)
-      resultingArray.push(addTag(node.tag, level, true, false)); // возвращаемся из глубины (закрываем теги)
+      resultingArray.push(createTag(node.tag, level, true, false)); // возвращаемся из глубины (закрываем теги)
     }
   }
 
